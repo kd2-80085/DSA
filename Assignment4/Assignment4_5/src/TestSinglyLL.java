2cdbfa5dfcaa14c2ae014faaa30682c517bbe5ba
chaitanya <chaitanyabhavar2021@gmail.com>
2023-10-31 00:00:43 +0530

    Assignment4 Uploaded

---
+++ Assignment4/Assignment4_5/src/TestSinglyLL.java
@@ -0,0 +1,21 @@
+/* In singly linear list implement following operations.
+	i. insert a new node after a given node
+	ii. insert a new node before a given node*/
+public class TestSinglyLL {
+
+	public static void main(String[] args) {
+
+		SinglyLL linkedList=new SinglyLL();
+		linkedList.addFirst(10);
+		linkedList.addFirst(20);
+		linkedList.addFirst(30);
+		linkedList.addFirst(40);
+		linkedList.addFirst(50);
+		
+		
+		
+
+
+	}
+
+}
