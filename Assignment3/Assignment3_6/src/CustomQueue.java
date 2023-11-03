6fb5d0295cd270472a04b7b3edc8129f5c6327d6
chaitanya <chaitanyabhavar2021@gmail.com>
2023-11-03 23:04:28 +0530

    Assignment3 Updated

---
+++ Assignment3/Assignment3_6/src/CustomQueue.java
@@ -0,0 +1,61 @@
+
+public class CustomQueue {
+    private int arr[];
+    private int front, rear;
+    private final int SIZE;
+
+    public CustomQueue(int size) {
+        SIZE = size;
+        arr = new int[SIZE];
+        front = -1;
+        rear = -1;
+    }
+
+    public void enqueue(int data) {
+        if (isFull()) {
+            throw new IllegalStateException("Queue is full");
+        }
+        if (isEmpty()) {
+            front = rear = 0;
+        } else {
+            rear = (rear + 1) % SIZE;
+        }
+        arr[rear] = data;
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        int data = arr[front];
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            front = (front + 1) % SIZE;
+        }
+        return data;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return arr[front];
+    }
+
+    public int size() {
+        if (isEmpty()) {
+            return 0;
+        }
+        return (rear - front + SIZE) % SIZE + 1;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % SIZE == front;
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+}
+
